b88f5ac8c0684010e192f75c99ddac1d2ed878ab
zach-a-novak <44446788+zach-a-novak@users.noreply.github.com>
2018-11-04 23:09:28 -0600

    Database, Credentials, InitializeDB
    PHP files for connecting to the database

---
+++ php/database.php
@@ -0,0 +1,33 @@
+<?php
+
+  require_once('dbCredentials.php');
+
+  /* Connect to the database with the credentials given in the file above
+     Return a handle to the PDO instance or output an error message and exit (stop execution)
+   */
+  function db_connect() 
+  {
+	  try 
+	  {
+		  $db = new PDO("mysql:dbname=" . DB_NAME . ";host=" . DB_SERVER, DB_USER, DB_PWD, 
+			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	  } 
+	  catch (PDOException $ex) 
+	  {
+		  die("Unable to connect to database.");
+	  }
+	  
+	  return $db;
+  }
+
+  /* disconnect from the database, if needed
+   */
+  function db_disconnect() 
+  {
+
+    global $db;
+	$db = null;
+
+  }
+
+ ?>
---
+++ php/dbCredentials.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_NAME", "team2");
+define("DB_USER", "team2");
+define("DB_PWD", "x670");
+
+?>
