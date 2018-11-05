90a90587505bb6b03339236aacaea21a4e5b1c52
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-05 15:48:08 -0600

    Update php/index.php

--- php/index.php
+++ php/index.php
@@ -4,8 +4,8 @@
 		<title>Biggest Loser</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/style.css" rel="stylesheet">
 	</head>
 	<body>
 		<!--Navbar container-->
