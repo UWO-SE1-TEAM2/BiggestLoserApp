fb25da0e56859654ebfdd595ed652fdaae43a39e
Nou Sue <Nou Sue@LAPTOP-PT6O316R>
2018-11-06 13:01:33 -0600

    Updates to login/signup
    Updated login and singup pages to center textbox and buttons and included header

--- php/_login.php
+++ php/_login.php
@@ -1,36 +1,42 @@
-<!--
-    Form needs to be styled
--->
 <!DOCTYPE html>
+
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Log In</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css"  href="account.css" />
-    
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+		<link rel="stylesheet" type="text/css"  href="mainStyle.css" />
     </head>
-        <body>
-            <form method="post">
-                <div class="wrapper">
-                    <h2>Login</h2>
-                    <p>Please fill in your credentials to login.</p>
-                    <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" name="username" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" name="password" class="form-control" required>
-                    </div>
-                    <div class="button">
-                        
-                        <input type="submit" class="btn btn-primary" value="Login">
-                    </div>
-                    
-                        <p>Don't have an account? <a href="_signup.php">Sign up now</a>.</p>
-                </div>
-            </form>
-        </body>
-</html>
 
+    <body>
+		<?php include_once 'header.php';?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+		          	<h2 class="text-center biggestLoser">Biggest Loser</h2>
+		          	<p class="text-center">Please fill in your credentials to login.</p>
+		 	  		<form method="post">
+		        	<div class="form-group">
+		                    <label>Username</label>
+		                    <input type="text" name="username" class="form-control" required>
+		                </div>
+		                <div class="form-group">
+		                    <label>Password</label>
+		                    <input type="password" name="password" class="form-control" required>
+		                </div>
+		                <div class="form-group">
+		                    <input type="submit" class="btn btn-info form-control" value="Login">
+		                </div>
+		                    <p class="text-center">Don't have an account? <a href="_signup.php">Sign up now</a>.</p>
+		                </div>
+	              	</form>
+				</div>
+			</div>
+		</div>
+		<?php include_once 'footer.php';?>
+    </body>
+
+</html>
--- php/_signup.php
+++ php/_signup.php
@@ -5,41 +5,49 @@
 -->
 <!DOCTYPE html>
 
-<head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Sign Up</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css"  href="account.css" />
-    
-    </head>
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Sign Up</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+		<link rel="stylesheet" type="text/css"  href="mainStyle.css" />
 
+	</head>
 
-<form  method="post">
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <div class="form-group">
-            <label>Username</label>
-            <input id="username" type="text" name="username" class="form-control" required>
-            <span id="usernameErr"></span>
-        </div>
-        <div class="form-group">
-            <label>Password</label>
-            <input id="password" type="password" name="password" class="form-control" required>
-            <span id="passwordErr"></span>
-        </div>
-        <div class="form-group">
-            <label class="confirm_password">Confirm Password</label>
-            <input id="confirm_password" type="password" name="confirm_password" class="form-control" required>
-            <span id="confirmErr"></span>
-        </div>
-        <div class="form-group button">
-            <input type="submit" class="btn btn-primary" value="Register">
-        </div>
-        <p>Already have an account? <a href="_login.php">Login here</a>.</p>
-    </div>
-</form>
+	<body>
+		<?php include_once 'header.php';?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3">
+					<h2 class="text-center biggestLoser">Biggest Loser</h2>
+					<p class="text-center">Please fill this form to create an account.</p>
+					<form  method="post">
+				        <div class="form-group">
+				            <label>Username</label>
+				            <input id="username" type="text" name="username" class="form-control" required>
+				            <span id="usernameErr"></span>
+				        </div>
+				        <div class="form-group">
+				            <label>Password</label>
+				            <input id="password" type="password" name="password" class="form-control" required>
+				            <span id="passwordErr"></span>
+				        </div>
+				        <div class="form-group">
+				            <label class="confirm_password">Confirm Password</label>
+				            <input id="confirm_password" type="password" name="confirm_password" class="form-control" required>
+				            <span id="confirmErr"></span>
+				        </div>
+				        <div class="form-group">
+				            <input type="submit" class="btn btn-info form-control" value="Register">
+				        </div>
+				        <p class="text-center">Already have an account? <a href="_login.php">Login here</a>.</p>
+					</form>
+				</div>
+			</div>
+		</div>
+		<?php include_once 'header.php'?>
+	</body>
 </html>
-
-
