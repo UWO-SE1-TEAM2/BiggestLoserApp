4b5c9ca3880503ef708f7b46cd5f669b63522dc6
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-11-07 14:58:22 -0600

    created adminQueries & reorganized DB phps

---
+++ php/DB/Queries/adminQueries.php
@@ -0,0 +1,27 @@
+<?php
+    function insertAdminToGroup($username, $group){
+        global $db;
+        try{
+            $query = "INSERT INTO Admin VALUES (?, ?)";
+            $stmt = $db->prepare($query);
+            $stmt->execute([$username, $group]);
+            return true;
+        } catch (PDOException $e){
+            db_disconnect();
+            exit("Aborting: There was a database error when inserting admin to group.");
+        }
+    }
+
+    function deleteAdminFromGroup($username, $group){
+        global $db;
+        try{
+            $query = "DELETE FROM Admin WHERE Username = $username, Group = $group";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e){
+            db_disconnect();
+            exit("Aborting: There was a database error when deleteing admin from group.");
+        }
+    }
+?>
