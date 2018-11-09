41f4912cf5f0a306268aba2120b7da6e36e30b5f
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-11-09 11:34:37 -0600

    created group querires.  Unsure if we want one update function or just split into multiple functions (ex updateWinner, updateName, etc.)

--- php/DB/Queries/groupQueries.php
+++ php/DB/Queries/groupQueries.php
@@ -1,9 +1,40 @@
 <?php
-    function InsertGroup(){
-
+    function InsertGroup($name){
+        global $db;
+        try{
+            $query = "INSERT INTO Group VALUES (?)";
+            $stmt = $db->prepare($query);
+            $stmt->execute([$name]);
+            return true;
+        } catch (PDOException $e){
+            db_disconnect();
+            exit("Aborting: There was a database error when inserting group.");
+        }
     }
 
-    function ArchiveGroup(){
-        
+    function ArchiveGroup($name){
+        global $db;
+        try{
+            $query = "UPDATE Group SET IsArchived = 1 WHERE Name = $name";
+            $stmt = $db->prepare($query);
+            $stmt->execute([$name]);
+            return true;
+        } catch (PDOException $e){
+            db_disconnect();
+            exit("Aborting: There was a database error when archiving group.");
+        }
+    }
+
+    function GetAllGroups(){
+        global $db;
+        try{
+            $query = "SELECT * FROM Group";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e){
+            db_disconnect();
+            exit9"aborting: There was a database error when retrieving groups.");
+        }
     }
 ?>
