0dd50883ae4b7a301fbd4875cfc245baf119b95f
Mai Nou <yangm89@uwosh.edu>
2018-11-25 21:07:00 -0600

    updated for group admin pages

--- php/groupAdmin.php
+++ php/groupAdmin.php
@@ -1,6 +1,10 @@
 <button type="button" id="addAdmin" class="btn btn-primary" data-toggle="modal"
 	data-target="#addAdminDiv">
-	Add Group admin
+	Add Group Admin
+</button>
+<button type="button" id="deleteAdmin" class="btn btn-primary" data-toggle="modal"
+	data-target="#deleteAdminDiv">
+	Delte Group Admin
 </button>
 <button type="button" id="deleteUser" class="btn btn-primary" data-toggle="modal"
 	data-target="#deleteUserDiv">
@@ -24,7 +28,38 @@
 						<select name="selectGroupAdmin">
 
 						</select>
-						<input class="btn btn-info form-control" name="btnAdddmin" type="submit" value="Add Admin">
+						<input class="btn btn-info form-control" name="btnAddAdmin" id="btnAddAdmin"
+						 	type="submit" value="Add Admin">
+					</div>
+					<!--Takes user to group page-->
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade" id="deleteAdminDiv" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h3>Delete Admin</h3>
+			</div>
+			<div class="modal-body">
+				<form method="post" action="groupHomePage.php"><!--Sends info to database-->
+					<div class="form-group" class="form-control" size="10">
+						<!--TODO: generate drop down with group admins usernames-->
+						<select name="selectDeleteAdmin">
+							<?php
+								for($i = 0; $i < count($admins[0]); $i++)
+								{
+									print "<option value=" . $admins[0]['Username'] . ">";
+									print $admins[0]['Username'];
+									print "</option>";
+								}
+							 ?>
+						</select>
+						<input class="btn btn-info form-control" name="btnDeleteAdmin" id="btnDeleteAdmin"
+						 	type="submit" value="Delete Admin">
 					</div>
 					<!--Takes user to group page-->
 				</form>
@@ -46,7 +81,8 @@
 						<select name="selectDeleteUser" class="form-control" size="10">
 
 						</select>
-						<input class="btn btn-info form-control" name="btnDeleteUser" type="submit" value="Delete User">
+						<input class="btn btn-info form-control" name="btnDeleteUser"
+						 	id="btnDeleteUser" type="submit" value="Delete User">
 					</div>
 					<!--Takes user to group page-->
 				</form>
@@ -67,7 +103,8 @@
 						<!--TODO: generate drop down with group members usernames-->
 						<label for="newEndDate">Update End Date</label>
 						<input id="newEndDate" name="newEndDate" type="date">
-						<input class="btn btn-info form-control" name="updateAdmin" type="submit" value="updateEndDate">
+						<input class="btn btn-info form-control" name="btnUpdateAdmin" id="btnUpdateAdmin"
+							type="submit" value="updateEndDate">
 					</div>
 					<!--Takes user to group page-->
 				</form>
