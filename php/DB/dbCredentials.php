d4411604d45ed292df3c53f7a3cf73e71a1d21f2
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-11-28 14:09:38 -0600

    have to db Credentials one for unit testing locally and one for the the webapplication

--- php/DB/dbCredentials.php
+++ php/DB/dbCredentials.php
@@ -1,8 +1,15 @@
 <?php
 
+/*for running webapplication*/
 define("DB_SERVER", "localhost");
 define("DB_NAME", "SWTeam2");
 define("DB_USER", "SWTeam2");
 define("DB_PWD", "j670");
+
+/* for Unit testing only*/
+// define("DB_SERVER", "localhost");
+// define("DB_NAME", "swteam2");
+// define("DB_USER", "root");
+// define("DB_PWD", "j670");
 
 ?>
