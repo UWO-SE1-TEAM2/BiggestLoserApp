069bebc88faf56088e34fdcb46e62bf6236ea7fa
Mai Nou <yangm89@uwosh.edu>
2018-11-27 23:42:30 -0600

    Update navigation bar
    Got rid of navigation bar drop down for the groups - should be sufficient to have groups on the home page.

--- php/navigationBar.php
+++ php/navigationBar.php
@@ -5,12 +5,6 @@
 		<div class="collapse navbar-collapse navHeaderCollapse">
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="home.php">Home</a></li>
-				<li class="dropdown">
-					<a href="" class="dropdown-toggle" data-toggle="dropdown">Groups <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="groupHomePage.php">Group name</a></li><!--Link to group page-->
-						</ul>
-				</li>
 				<li><a href="changePassword.php">Change Password</a></li>     <!--Link to login page-->
 				<li><a href="logout.php">Logout</a></li>     <!--Link to login page-->
 			</ul>
